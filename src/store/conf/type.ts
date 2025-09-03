df80545b7a9b208dd27f1cf696844e727262e9a1
obrucheoghene <obrucheoghene1@gmail.com>
2025-09-03 17:14:50 +0100

    implement camera slice

--- src/store/conf/type.ts
+++ src/store/conf/type.ts
@@ -1,7 +1,9 @@
+import type { CameraSlice } from './slices/camera-slice';
 import type { CounterSlice } from './slices/counter-slice';
 import type { MicSlice } from './slices/mic-slice';
 
 export interface ConfStoreState {
   counter: CounterSlice;
   mic: MicSlice;
+  camera: CameraSlice;
 }
