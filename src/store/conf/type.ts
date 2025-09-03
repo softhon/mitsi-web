ad0f513655e249745f9ed2fbf1b0af255a498fc3
obrucheoghene <obrucheoghene1@gmail.com>
2025-09-03 17:12:57 +0100

    add mic slice

--- src/store/conf/type.ts
+++ src/store/conf/type.ts
@@ -1,5 +1,7 @@
 import type { CounterSlice } from './slices/counter-slice';
+import type { MicSlice } from './slices/mic-slice';
 
 export interface ConfStoreState {
   counter: CounterSlice;
+  mic: MicSlice;
 }
