932199f0c655f812e35173fe2b5cd2607f5b0399
obrucheoghene <obrucheoghene1@gmail.com>
2025-09-18 11:08:34 +0100

    clean up conference

--- src/pages/room/conference.tsx
+++ src/pages/room/conference.tsx
@@ -1,57 +1,10 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 
 import ControlBar from '@/components/conference/control-bar';
 import Header from '@/components/conference/header';
 import { ConferenceGrid } from '@/components/conference/grid/conference-grid';
 
-import { usePeerActions, usePeerMe, useRoomData } from '@/store/conf/hooks';
-import { useSignaling } from '@/hooks/use-signaling';
-import { useMedia } from '@/hooks/use-media';
-import { Actions } from '@/types/actions';
-import type { PeerData, RoomData } from '@/types';
-
 export const Conference: React.FC = () => {
-  const peerMe = usePeerMe();
-  const roomData = useRoomData();
-  const { signalingService } = useSignaling();
-  const { mediaService } = useMedia();
-  const peerActions = usePeerActions();
-  const [chatOn, setChatOn] = useState(false);
-
-  const toggleChat = useCallback(() => {
-    setChatOn(prev => !prev);
-  }, [chatOn]);
-
-  // const joinRoom = async () => {};
-
-  useEffect(() => {
-    const setup = async () => {
-      if (!peerMe || !signalingService || !mediaService) return;
-      try {
-        const result = await signalingService.message<{
-          peers: PeerData[];
-          roomData: RoomData;
-        }>({
-          action: Actions.JoinRoom,
-          args: {
-            roomId: roomData?.roomId,
-            peerData: peerMe,
-            rtpCapabilities: mediaService.getDeviceRtpCapabilities(),
-          },
-        });
-
-        const peers = result?.peers;
-        peerActions.clear();
-        if (peers) {
-          peerActions.addOthersData(peers);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    setup();
-  }, []);
-
   return (
     <div className=" relative h-screen bg-gray-900 flex flex-col overflow-hidden justify-between">
       {/* Header */}
@@ -61,7 +14,7 @@ export const Conference: React.FC = () => {
       <ConferenceGrid />
 
       {/* Bottom Controls */}
-      <ControlBar onToggleChat={toggleChat} />
+      <ControlBar onToggleChat={() => {}} />
     </div>
   );
 };
