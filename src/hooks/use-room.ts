0a835702a38d524e80c55b5c0588618ed61b2f3c
obrucheoghene <obrucheoghene1@gmail.com>
2025-09-18 11:07:50 +0100

    implement use room to handle

---
+++ src/hooks/use-room.ts
@@ -0,0 +1,112 @@
+import {
+  usePeerActions,
+  usePeerMe,
+  useRoomAccess,
+  useRoomData,
+} from '@/store/conf/hooks';
+import { useCallback, useMemo } from 'react';
+import {
+  Access,
+  type AckCallbackData,
+  type PeerData,
+  type RoomData,
+} from '@/types';
+import { Actions } from '@/types/actions';
+import { useSignaling } from './use-signaling';
+import { useMedia } from './use-media';
+import { ValidationSchema } from '@/lib/schema';
+
+export const useRoom = () => {
+  const { signalingService } = useSignaling();
+  const {
+    mediaService,
+    createConsumer,
+    pauseConsumer,
+    closeConsumer,
+    resumeConsumer,
+  } = useMedia();
+  const peerActions = usePeerActions();
+  const peerMe = usePeerMe();
+  const roomData = useRoomData();
+  const roomAccess = useRoomAccess();
+
+  const joinRoom = useCallback(
+    async (isRejoining?: boolean) => {
+      if (roomAccess !== Access.Allowed) return;
+      if (!peerMe || !roomData?.roomId || !signalingService || !mediaService)
+        return;
+      const result = await signalingService.message<{
+        peers: PeerData[];
+        roomData: RoomData;
+      }>({
+        action: Actions.JoinRoom,
+        args: {
+          roomId: roomData.roomId,
+          peerData: peerMe,
+          rtpCapabilities: mediaService.getDeviceRtpCapabilities(),
+          isRejoining,
+        },
+      });
+
+      const peers = result?.peers;
+      peerActions.clear();
+      if (peers) {
+        peerActions.addOthersData(peers);
+      }
+    },
+    [
+      peerMe,
+      signalingService,
+      mediaService,
+      roomAccess,
+      peerActions,
+      roomData?.roomId,
+    ]
+  );
+
+  const actionHandlers: {
+    [key in Actions]?: (
+      args: { [key: string]: unknown },
+      callback: (data: AckCallbackData) => void
+    ) => void;
+  } = useMemo(
+    () => ({
+      [Actions.PeerAdded]: async args => {
+        const data = ValidationSchema.peerData.parse(args);
+        peerActions.addData(data);
+      },
+
+      [Actions.PeerLeft]: async args => {
+        const data = ValidationSchema.peerId.parse(args);
+        peerActions.remove(data.id);
+      },
+
+      [Actions.ConsumerCreated]: async (args, callback) => {
+        const data = ValidationSchema.createConsumerData.parse(args);
+        await createConsumer(data);
+        callback({ status: 'success' });
+        console.log(Actions.ConsumerCreated, data);
+        callback({ status: 'success' });
+      },
+
+      [Actions.ConsumerPaused]: async args => {
+        const data = ValidationSchema.consumerStateData.parse(args);
+        pauseConsumer(data);
+        console.log(Actions.ConsumerPaused, data);
+      },
+      [Actions.ConsumerResumed]: async args => {
+        const data = ValidationSchema.consumerStateData.parse(args);
+        resumeConsumer(data);
+        console.log(Actions.ConsumerResumed, data);
+      },
+      [Actions.ConsumerClosed]: async args => {
+        const data = ValidationSchema.consumerStateData.parse(args);
+        closeConsumer(data);
+        console.log(Actions.ConsumerPaused, data);
+      },
+    }),
+    [closeConsumer, createConsumer, pauseConsumer, peerActions, resumeConsumer]
+  );
+
+  return { joinRoom, actionHandlers };
+};
