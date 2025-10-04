061787ffd791724d4154dff6e87ef7fe05003fe3
obrucheoghene <obrucheoghene1@gmail.com>
2025-10-04 15:49:37 +0100

    rename app file

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './app';
+import App from './app1';
 
 createRoot(document.getElementById('root')!).render(<App />);
