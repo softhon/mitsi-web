e8f81ac497efd3ece37c20d0c3c612c5d0cb7e10
obrucheoghene <obrucheoghene1@gmail.com>
2025-11-04 11:03:27 +0100

    update

--- src/components/room/hand.tsx
+++ src/components/room/hand.tsx
@@ -12,7 +12,7 @@ const Hand = () => {
       variant="ghost"
       size="icon"
       className={cn(
-        'w-12 h-12 rounded-xl transition-all duration-200 bg-gradient-to-br text-white',
+        'w-12 h-12 rounded-xl transition-all duration-200 bg-gradient-to-bl text-white',
         isHandRaised
           ? 'bg-yellow-600 hover:bg-yellow-700 '
           : ' from-white/15 to-white/1  backdrop-blur-xl'
