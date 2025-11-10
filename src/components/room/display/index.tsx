f00ff02a7db667339048675a2a23f882b5b5fea8
obrucheoghene <obrucheoghene1@gmail.com>
2025-11-10 19:55:33 +0100

    update display sizing

--- src/components/room/display/index.tsx
+++ src/components/room/display/index.tsx
@@ -5,8 +5,9 @@ import ScreenView from './screen-view';
 const Display = () => {
   return (
     <div
-      className={`${'fixed h-full inset-0  pt-10 pb-18 px-3 flex  flex-row w-full justify-between overflow-hidden '}`}
+      className={`${'fixed h-full inset-0  pt-4 pb-18 px-3 flex  flex-row w-full justify-between overflow-hidden '}`}
     >
+      {/* change padding to pt-10 */}
       {/* screen sharing */}
       <ScreenView />
       {/* peers grid */}
