e2d22b8e0b0c4f66e18a9b4fddc3ba6029c4c757
obrucheoghene <obrucheoghene1@gmail.com>
2025-11-10 19:55:16 +0100

    add coming soon

--- src/components/room/menu.tsx
+++ src/components/room/menu.tsx
@@ -7,8 +7,9 @@ const Menu = () => {
       variant="ghost"
       size="icon"
       className="w-12 h-12 rounded-xl  text-white
-      bg-gradient-to-tl from-white/15 to-white/1  backdrop-blur-xl
+      bg-linear-to-tl from-white/15 to-white/1  backdrop-blur-xl
       "
+      title="Coming soon"
     >
       <MenuIcon className="w-5 h-5" />
     </Button>
