e3c30d567858fc419f5af294dd5eb4ed577ed846
obrucheoghene <obrucheoghene1@gmail.com>
2025-11-24 01:52:47 +0100

    feat: add menu dropdown

--- src/components/room/menu.tsx
+++ src/components/room/menu.tsx
@@ -1,18 +1,51 @@
 import { Button } from '../ui/button';
-import { Menu as MenuIcon } from 'lucide-react';
+import {
+  Fullscreen,
+  Image,
+  Menu as MenuIcon,
+  MessageSquareWarning,
+  Settings,
+} from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
 
 const Menu = () => {
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      className="w-12 h-12 rounded-xl  text-white
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="w-12 h-12 rounded-xl  text-white
       bg-linear-to-tl from-white/15 to-white/1  backdrop-blur-xl
       "
-      title="Coming soon"
-    >
-      <MenuIcon className="w-5 h-5" />
-    </Button>
+          title="Coming soon"
+        >
+          <MenuIcon className="w-5 h-5" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-linear-to-bl from-slate-900 to-slate-800">
+        <DropdownMenuItem>
+          <Fullscreen /> Go Fullscreen
+        </DropdownMenuItem>
+
+        <DropdownMenuItem>
+          <Image /> Background Effect
+        </DropdownMenuItem>
+        <DropdownMenuSeparator></DropdownMenuSeparator>
+        <DropdownMenuItem>
+          <Settings /> Settings
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <MessageSquareWarning /> Report/Feedback
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
